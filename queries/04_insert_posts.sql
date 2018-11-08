159838cd6eca23c42e6ff5c6b674b09cd4150207
morthos <pathshan61@gmail.com>
2018-11-08 10:52:08 -0500

    .

---
+++ queries/04_insert_posts.sql
@@ -0,0 +1,5 @@
+INSERT INTO posts(title, body, user_id)
+VALUES('Admin announcement', "This API is higly functionable" user_id);
+
+INSERT INTO posts (title, body, user_id)
+VALUES('Just a test''This is just a test. I repeat, this is just a test.', user_id);
